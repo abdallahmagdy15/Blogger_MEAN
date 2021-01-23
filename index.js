fb53e293ff56b4d13c8a6a0a5eeccfab0e472d6f
abdallahm15 <abdallah.magdy1515@gmail.com>
2021-01-23 22:54:20 +0200

    adding atlas url mongodb

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = require('./routes');
 const mongoose = require('mongoose');
 const app = express();
+const atlasMongodbUri = process.env.MONGODB_URI;
 
-mongoose.connect('mongodb://localhost:27017/blogsdb_iti', {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(atlasMongodbUri, {useNewUrlParser: true, useUnifiedTopology: true});
 /// make db indecies*****************
 
 app.use(express.json());
