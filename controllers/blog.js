488f304b1a33e0b99a165655ee7958228b7bd954
abdallahm15 <abdallah.magdy1515@gmail.com>
2021-01-24 22:49:19 +0200

    catch all errors in blogs controller queries

--- controllers/blog.js
+++ controllers/blog.js
@@ -21,17 +21,30 @@ const getBlogs = async (query, pagination, author) => {
         })
         console.log(...blogsIds)
         return blogModel.find(query).where('_id').in(blogsIds)
-            .limit(pagination.limit).skip(pagination.skip).exec();
+            .limit(pagination.limit).skip(pagination.skip).exec().then(function () {
+                console.log(`getBlogs works`);
+            }).catch(function (err) {
+                console.log(`caught the error in getBlogs: ${err}`);
+            });
     }
 }
 
 
 const getFollowingsBlogs = async (query, pagination, author, followingsIds) => {
     if (author == undefined)
-        return blogModel.find(query).sort([['updatedAt',-1]]).limit(pagination.limit).skip(pagination.skip).exec();
+        return blogModel.find(query).sort([['updatedAt',-1]]).limit(pagination.limit).skip(pagination.skip)
+        .exec().then(function () {
+            console.log(`getFollowingsBlogs works`);
+        }).catch(function (err) {
+            console.log(`caught the error in getFollowingsBlogs: ${err}`);
+        });
     else {
         return blogModel.find(query).where('_id').in(followingsIds)
-            .limit(pagination.limit).skip(pagination.skip).exec();
+            .limit(pagination.limit).skip(pagination.skip).exec().then(function () {
+                console.log(`getFollowingsBlogs works`);
+            }).catch(function (err) {
+                console.log(`caught the error in getFollowingsBlogs: ${err}`);
+            });
     }
 }
 
@@ -40,8 +53,17 @@ const getOneBlog = (id) => blogModel.findById(id).exec();
 const createBlog = async (res,blog) => {
     blog.createdAt = new Date()
     blog.updatedAt = new Date()
-    const _blog = await blogModel.create(blog)
-    await userModel.findByIdAndUpdate(blog.author, { $push: { blogs: _blog.id } }, { new: true }).exec()
+    const _blog = await blogModel.create(blog).then(function () {
+        console.log(`createBlog works`);
+    }).catch(function (err) {
+        console.log(`caught the error in createBlog: ${err}`);
+    });
+    await userModel.findByIdAndUpdate(blog.author, { $push: { blogs: _blog.id } }, { new: true })
+    .exec().then(function () {
+        console.log(`createBlog works`);
+    }).catch(function (err) {
+        console.log(`caught the error in createBlog: ${err}`);
+    });
     res.send(_blog)
 }
 //
@@ -49,14 +71,31 @@ const createBlog = async (res,blog) => {
 
 const updateBlog = (id, blogBody) => {
     blogBody.updatedAt = new Date()
-    return blogModel.findByIdAndUpdate(id, blogBody, { new: true }).exec()
+    return blogModel.findByIdAndUpdate(id, blogBody, { new: true })
+    .exec().then(function () {
+        console.log(`updateBlog works`);
+    }).catch(function (err) {
+        console.log(`caught the error in updateBlog: ${err}`);
+    });
 }
 
 const removeBlog = async (id) => {
-    const blog = await blogModel.findByIdAndDelete(id).exec()
+    const blog = await blogModel.findByIdAndDelete(id)
+    .exec().then(function () {
+        console.log(`removeBlog works`);
+    }).catch(function (err) {
+        console.log(`caught the error in removeBlog: ${err}`);
+    });
+
     if (id != blog.author)
-        return { "error": "Not Author" }
-    await userModel.findByIdAndUpdate(query.author, { $pull: { blogs: blog.id } }, { new: true }).exec()
+        throw new Error( "Denied : Can't delete the blog because you are not the author" )
+        
+    await userModel.findByIdAndUpdate(query.author, { $pull: { blogs: blog.id } }, { new: true })
+    .exec().then(function () {
+        console.log(`removeBlog works`);
+    }).catch(function (err) {
+        console.log(`caught the error in removeBlog: ${err}`);
+    });
     return blog
 }
 
