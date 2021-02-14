d55aac9e7cad64cfc8c2ee137aab378eb2bcf670
abdallahm15 <abdallah.magdy1515@gmail.com>
2021-02-14 20:08:42 +0200

    try fixing cors error

--- controllers/blog.js
+++ controllers/blog.js
@@ -6,7 +6,7 @@ const getBlogs = async (query, pagination) => {
 
     return blogModel.find(query).sort([['updatedAt', -1]])
         .limit(pagination.limit).skip(pagination.skip).exec().then().catch(e => {
-            throw new Error("Caught error in getBlogs :" + e.message)
+            throw new Error("Caught error in getBlogs :"+ e.message)
         })
 }
 
@@ -15,12 +15,12 @@ const getFollowingsBlogs = async (query, pagination, followingsIds) => {
     return blogModel.find(query).where('author').in(followingsIds).sort([['updatedAt', -1]])
         .limit(pagination.limit).skip(pagination.skip).exec()
         .then().catch(e => {
-            throw new Error("Caught error in getFollowingsBlogs :" + e.message)
+            throw new Error("Caught error in getFollowingsBlogs :"+ e.message)
         })
 }
 
 const getOneBlog = (id) => blogModel.findById(id).exec().then().catch(e => {
-    throw new Error("Caught error in getOneBlog :" + e.message)
+    throw new Error("Caught error in getOneBlog :"+ e.message)
 })
 
 const createBlog = async (blog) => {
@@ -28,12 +28,12 @@ const createBlog = async (blog) => {
     blog.updatedAt = new Date()
 
     const _blog = await blogModel.create(blog).then().catch(e => {
-        throw new Error("Caught error in createBlog :" + e.message)
+        throw new Error("Caught error in createBlog :"+ e.message)
     })
     //update user blogs
     userModel.findByIdAndUpdate(blog.author, { $push: { blogs: _blog.id } }, { new: true })
         .exec().then().catch(e => {
-            throw new Error("Caught error in createB log :" + e.message)
+            throw new Error("Caught error in createB log :"+ e.message)
         })
 
     return _blog;
@@ -48,7 +48,7 @@ const updateBlog = async (userblogsIds, blogid, blogBody) => {
     blogBody.updatedAt = new Date()
     return blogModel.findByIdAndUpdate(blogid, blogBody, { new: true })
         .exec().then().catch(e => {
-            throw new Error("Caught error in updateBlog :" + e.message)
+            throw new Error("Caught error in updateBlog :"+ e.message)
         })
 }
 
@@ -59,12 +59,12 @@ const removeBlog = async ({ blogs, id }, blogid) => {
         return { "status": "Denied , not authorized to delete this blog!" }
     const blog = await blogModel.findByIdAndDelete(blogid)
         .exec().then().catch(e => {
-            throw new Error("Caught error in removeBlog :" + e.message)
+            throw new Error("Caught error in removeBlog :"+ e.message)
         })
 
     await userModel.findByIdAndUpdate(id, { $pull: { blogs: blog.id } }, { new: true })
         .exec().then().catch(e => {
-            throw new Error("Caught error in removeBlog :" + e.message)
+            throw new Error("Caught error in removeBlog :"+ e.message)
         })
     return blog
 }
@@ -76,7 +76,7 @@ const createComment = async (blogid, comment) => {
 
     await blogModel.findByIdAndUpdate(blogid, { $push: { comments: comment } }, { new: true })
         .exec().then().catch(e => {
-            throw new Error("Caught error in createComment :" + e.message)
+            throw new Error("Caught error in createComment :"+ e.message)
         })
     return comment;
 }
@@ -91,7 +91,7 @@ const updateComment = async (uid, blogid, commentid, comment) => {
     blog.comments.push(cmnt);
     await blog.save(function (err) {
         if (err)
-            throw new Error("Caught error in updateComment :" + err.message)
+            throw new Error("Caught error in updateComment :"+ err.message)
     })
     return cmnt;
 }
@@ -106,7 +106,7 @@ const removeComment = async (uid, blogid, commentid) => {
 
     await blog.save(function (err) {
         if (err)
-            throw new Error("Caught error in updateComment :" + err.message)
+            throw new Error("Caught error in updateComment :"+ err.message)
     })
     return cmnt;
 }
@@ -114,7 +114,7 @@ const removeComment = async (uid, blogid, commentid) => {
 const likeBlog = async (uid, blogid) => {
     await blogModel.findByIdAndUpdate(blogid, { $addToSet: { likes: uid } }, { new: true })
         .exec().then().catch(e => {
-            throw new Error("Caught error in likeBlog :" + e.message)
+            throw new Error("Caught error in likeBlog :"+ e.message)
         })
     return {
         "status": "blog is liked !"
@@ -124,7 +124,7 @@ const likeBlog = async (uid, blogid) => {
 const unlikeBlog = async (uid, blogid) => {
     await blogModel.findByIdAndUpdate(blogid, { $pull: { likes: uid } }, { new: true })
         .exec().then().catch(e => {
-            throw new Error("Caught error in likeBlog :" + e.message)
+            throw new Error("Caught error in likeBlog :"+ e.message)
         })
     return {
         "status": "blog is unliked !"
@@ -133,15 +133,14 @@ const unlikeBlog = async (uid, blogid) => {
 
 const likeComment = async (uid, blogid, commentid) => {
     let blog = await blogModel.findById(blogid).exec();
-    let cmnt = blog.comments.id(commentid)
-    blog.comments.id(commentid).remove();
+    let cmnt = blog.comments.id(commentid).remove();
 
     cmnt.likes.addToSet(uid);
-    console.log(cmnt);
+
     blog.comments.push(cmnt);
     await blog.save(function (err) {
         if (err)
-            throw new Error("Caught error in likeComment :" + err.message)
+            throw new Error("Caught error in likeComment :"+ err.message)
     })
 
     return {
@@ -158,7 +157,7 @@ const unlikeComment = async (uid, blogid, commentid) => {
     blog.comments.push(cmnt);
     await blog.save(function (err) {
         if (err)
-            throw new Error("Caught error in unlikeComment :" + err.message)
+            throw new Error("Caught error in unlikeComment :"+ err.message)
     })
 
     return {
--- index.js
+++ index.js
@@ -11,12 +11,29 @@ mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true 
   .catch((e) => console.log('caught error while connecting to db : ', e));
 
 app.use(express.json());
-
-
 app.use(cors())
 router.options('/', cors())
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 // setup routes
-app.use('/',cors() , router);
+app.use('/', router);
 app.use('/test', (req, res) => {
   res.json({ "test": "success" })
 })
