b217a5145f1e070b3241eff8d658b5bc35b66514
abdallahm15 <abdallah.magdy1515@gmail.com>
2021-02-16 21:36:56 +0200

    blog schema

--- models/blog.js
+++ models/blog.js
@@ -2,28 +2,28 @@ const Mongoose = require('mongoose')
 const { Schema } = Mongoose
 
 const commentSchema = new Schema({
-        author: {
-            type: Schema.Types.ObjectId,
-            ref: 'User',
-            required: true
-        },
-        authorDp: String,
-        authorName: { type: String, required: true },
-        body: {
-            type: String,
-            maxlength: 1024,
-            required: true
-        },
-        createdAt: { type: Date, required: true },
-        updatedAt: { type: Date, required: true },
-        photo: String,
-        likes: [{type:Schema.Types.ObjectId, ref: 'user'}]
-    });
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    authorDp: String,
+    authorName: { type: String, required: true },
+    body: {
+        type: String,
+        maxlength: 1024,
+        required: true
+    },
+    createdAt: { type: Date, required: true },
+    updatedAt: { type: Date, required: true },
+    photo: String,
+    likes: [{ type: Schema.Types.ObjectId, ref: 'user' }]
+});
 
 const blogSchema = new Schema({
     author: { type: Schema.Types.ObjectId, ref: 'user', required: true },
-    title: { type: String, required: true, maxLength: 60 },
-    body: { type: String, required: true, maxLength: 2100 },
+    title: { type: String, required: true, maxLength: 160 },
+    body: { type: String, required: true, maxLength: 6000 },
     authorDp: String,
     authorName: { type: String, required: true },
     photo: String,
@@ -31,7 +31,7 @@ const blogSchema = new Schema({
     createdAt: { type: Date, required: true },
     updatedAt: { type: Date, required: true },
     comments: [commentSchema],
-    likes: [{type:Schema.Types.ObjectId, ref: 'user'}]
+    likes: [{ type: Schema.Types.ObjectId, ref: 'user' }]
 })
 
 const blogModel = Mongoose.model('blog', blogSchema)
